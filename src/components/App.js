a1a7b9c780fb8de9b54f6b4d289c59ba310673bb
sujitbidawe <bidawesujit@gmail.com>
2022-09-14 00:23:41 +0530

    ADDED: code to receive tweets in the dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from '../actions/shared';
+import Dashboard from "./Dashboard";
 
 const App = (props) => {
 	useEffect(() => {
 		props.dispatch(handleInitialData());
 	}, [])
 
-	return <div>Starter Code</div>;
+	return <div>
+		{
+			props.loading === true ? null : <Dashboard />
+		}
+	</div>;
 };
+
+const mapStateToProps = ({authedUser}) => ({
+		loading: authedUser === null
+})
 
 export default connect()(App);
