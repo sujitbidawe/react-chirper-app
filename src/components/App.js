b7f274cb8f4ce4df884a729d1d959849d0b7820a
sujitbidawe <bidawesujit@gmail.com>
2022-09-13 23:42:40 +0530

    ADDED: code to fetch initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>Starter Code</div>;
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from '../actions/shared';
+
+const App = (props) => {
+	useEffect(() => {
+		props.dispatch(handleInitialData());
+	}, [])
+
+	return <div>Starter Code</div>;
 };
 
-export default App;
+export default connect()(App);
