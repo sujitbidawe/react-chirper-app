3af348433a572c34fedaf0872844365f7e5e1737
sujitbidawe <bidawesujit@gmail.com>
2022-09-14 00:22:34 +0530

    RESOLVED: default state issue

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -8,8 +8,6 @@ export default function tweets(state = {}, action) {
                 ...action.tweets
             }
         default:
-            return {
-                state
-            }
+            return state
     }
 }
